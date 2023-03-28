057fec443ba340fb5850c4d824180a1ac7001259
Eugene-Lasevich <lasevich009@gmail.com>
2023-03-28 19:52:46 +0300

    Fixed task2

---
+++ lab2/task2/main.py
@@ -0,0 +1,36 @@
+from my_container import *
+
+storage = UserStorage()
+while True:
+    username = input("Enter username: ")
+    user_container = storage.switch_user(username)
+
+    while True:
+        command = input("Enter command: ")
+        match command:
+            case "add":
+                keys = input("Enter keys separated by space: ").split()
+                user_container.add(*keys)
+            case "remove":
+                key = input("Enter key to remove: ")
+                user_container.remove(key)
+            case "find":
+                keys = input("Enter keys separated by space: ").split()
+                user_container.find(*keys)
+            case "show":
+                user_container.show()
+            case "grep":
+                regex = input("Enter regular expression: ")
+                user_container.grep(regex)
+            case "save":
+                user_container.save()
+            case "load":
+                user_container.load()
+            case "switch":
+                break
+            case "exit":
+                storage.save_all()
+                print("Exiting...")
+                exit(0)
+            case _:
+                print("Invalid command, try again")
