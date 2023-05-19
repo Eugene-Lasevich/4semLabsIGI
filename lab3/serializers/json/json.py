9d209b1b40900d04ef64a64a6143b4fb3355f718
Eugene-Lasevich <lasevich009@gmail.com>
2023-05-19 15:34:10 +0300

    Add json serialization

---
+++ lab3/serializers/json/json.py
@@ -0,0 +1,31 @@
+import sys
+from serializers.dict_serilizer.functions import deseralize, serealize
+from serializers.json.parse_json import parse_json
+
+
+class Json:
+    @staticmethod
+    def dumps(obj):
+        obj = str(serealize(obj))
+
+        return obj
+
+    @staticmethod
+    def dump(obj, file_name):
+        f = open(file_name, 'w+')
+        f.truncate()
+        f.write(Json.dumps(obj))
+
+    @staticmethod
+    def loads(str):
+        tmp = parse_json(str)
+
+        return deseralize(tmp)
+
+    @staticmethod
+    def load(file_name):
+        f = open(file_name, 'r+')
+        str = f.read()
+        return Json.loads(str)
+
+
