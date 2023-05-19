92917632519d588ee8fda3edf4cb396da926fc94
Eugene-Lasevich <lasevich009@gmail.com>
2023-05-19 18:09:55 +0300

    Add xml serialization

---
+++ lab3/serializers/xml/xml.py
@@ -0,0 +1,32 @@
+from serializers.xml.xml_parser import dumps_from_dict, loads_to_dict
+from serializers.dict_serilizer.functions import serealize, deseralize
+
+
+class Xml:
+    @staticmethod
+    def dumps(obj):
+        obj = serealize(obj)
+
+        return dumps_from_dict(obj)
+
+    @staticmethod
+    def loads(obj):
+        obj = loads_to_dict(obj)
+
+        return deseralize(obj)
+
+    @staticmethod
+    def dump(obj, file_name):
+        f = open(file_name, 'w+')
+        f.truncate()
+        f.write(Xml.dumps(obj))
+
+    @staticmethod
+    def load(file_name):
+        f = open(file_name, 'r+')
+        str = f.read()
+        return Xml.loads(str)
+
+
+
+
